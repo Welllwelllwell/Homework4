ee7d20243c72c5bb311da596c22cd07fe907648d
«Max» <maks28isaev@gmail.com>
2023-06-25 11:50:53 +0300

    dz

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+System.Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Lenght(numberA);
+for (int i = 0; i < Length(numberA); i++)
+{
+    numberB = +0;
+    System.Console.WriteLine();
+}
---
+++ task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+System.Console.WriteLine("Введите число 8");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA == 8)
+{
+    int[] array = new int[numberA];
+
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        Console.Write(array[i] + " ");
+    }
+}
+else
+{
+    System.Console.WriteLine("Ввели число больше 8");
+}
